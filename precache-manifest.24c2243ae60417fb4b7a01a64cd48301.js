0afe4a6209a7ebb166ac929ad51b91c0da14ccf7
Adarsh2805-CMD <patra.adarsh9@gmail.com>
2020-06-30 22:47:46 +0530

    Updates

---
+++ precache-manifest.24c2243ae60417fb4b7a01a64cd48301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33a2cbce2897962af8b42078a84ac73",
+    "url": "/Adarsh2805-cmd/rf/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Adarsh2805-cmd/rf/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bec98bb2b84968a8ba62",
+    "url": "/Adarsh2805-cmd/rf/static/css/main.b1911f59.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Adarsh2805-cmd/rf/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Adarsh2805-cmd/rf/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec98bb2b84968a8ba62",
+    "url": "/Adarsh2805-cmd/rf/static/js/main.136bf429.chunk.js"
+  },
+  {
+    "revision": "7d0d2dae1f3c8a55c52d",
+    "url": "/Adarsh2805-cmd/rf/static/js/runtime-main.16846a63.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Adarsh2805-cmd/rf/static/media/SEGA.12a3db8b.woff"
+  }
+]);
