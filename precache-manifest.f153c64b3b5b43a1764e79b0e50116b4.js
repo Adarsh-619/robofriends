b3d96332f8d89ee9dd3e511521cf27bf3d824f6d
Adarsh2805-CMD <patra.adarsh9@gmail.com>
2020-06-30 22:51:32 +0530

    Updates

---
+++ precache-manifest.f153c64b3b5b43a1764e79b0e50116b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f0d027c89c791bb147c2c6bb16ec91",
+    "url": "/Adarsh2805-cmd/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Adarsh2805-cmd/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "788cf53e8c739ae5fcea",
+    "url": "/Adarsh2805-cmd/robofriends/static/css/main.4d18a7e2.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Adarsh2805-cmd/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Adarsh2805-cmd/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788cf53e8c739ae5fcea",
+    "url": "/Adarsh2805-cmd/robofriends/static/js/main.622c3db9.chunk.js"
+  },
+  {
+    "revision": "ef973116a051989b021b",
+    "url": "/Adarsh2805-cmd/robofriends/static/js/runtime-main.ef8b4261.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Adarsh2805-cmd/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
